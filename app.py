4dd2e8f21b5244d01476380177bf3b4edfdeefe5
Areen Jain <areenjain113@gmail.com>
2025-03-01 19:58:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 import streamlit as st
 from resume_parser import parse_resume
 from job_fetcher import fetch_jobs
-from ats_scoring import scoring, job_profiles, experience, tailored_resume
+from ats_scoring import scoring, job_profiles,extract_experience, tailored_resume,linkedin_optimization
 import pdfkit
 import os
-
-
-import subprocess
-
-# Grant execution permissions using subprocess
-subprocess.run(["chmod", "+x", os.path.join(os.getcwd(), "bin", "wkhtmltopdf")])
-
-# Path to the locally extracted wkhtmltopdf binary
-WKHTMLTOPDF_PATH = os.path.join(os.getcwd(), "bin", "wkhtmltopdf")
-
-# Configure pdfkit to use the local binary
-config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_PATH)
-
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
+st.set_page_config(page_title="BestFit.AI", page_icon="🚀", layout="wide")
 
 # Page Title
 st.markdown("<h1 style='text-align: center; color: #4CAF50;'>🚀 BestFit.AI</h1>", unsafe_allow_html=True)
@@ -39,6 +27,7 @@ st.markdown("""
 🔍 **Find the Best Jobs** – AI matches your resume with top job listings & provides direct apply links.  
 📊 **ATS Score & Resume Optimization** – Get an ATS score & AI-powered improvement suggestions.  
 📝 **Tailored Resume Generator** – Upload job descriptions & generate an optimized resume instantly.  
+🔗 **LinkedIn Profile Optimizer** – Enhance your LinkedIn profile with AI-driven suggestions for better visibility.  
 """, unsafe_allow_html=True)
 
 # How It Works
@@ -47,25 +36,34 @@ st.markdown("""
 1️⃣ **Find Jobs** – Upload resume & get matched job listings with apply links.  
 2️⃣ **Optimize Resume** – Upload job descriptions & improve ATS compatibility.  
 3️⃣ **Generate Tailored Resume** – AI rewrites & optimizes your resume for the job.  
+4️⃣ **Optimize LinkedIn Profile** – Get AI-powered insights to enhance your LinkedIn profile.  
 """, unsafe_allow_html=True)
+
     
 # User Selection Box
 select = st.selectbox(
     "What would you like to do?",
-    ["Select an option", "🔍 Find a Job", "📊 Get ATS Score","🤖AI-Powered Resume Customization"]
+    ["Select an option", "🔍 Find a Job", "📊 ATS Insights","🤖AI-Optimized Resume","🚀LinkedIn Profile Optimizer"]
 )
 
 #for finding jobs based on resume
 if select == "🔍 Find a Job":
+    st.title("🔍 Find a Job")
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf", "docx"])
     remote_only=st.selectbox("Remote Only",options=["False","True"])
-    employment_type=st.selectbox("Employment Type",options=["Full-time","Part-time","Contract","Internship"])
+    employment_type=st.selectbox("Employment Type",options=["fulltime","parttime","contract","internship"])
     job_location=st.text_input("Enter Job Location")
+    go=st.button("Find Jobs")
+    if not uploaded_file and go:
+        st.warning("Please upload your resume to find jobs.")
+    if not job_location and go:
+        st.warning("Please enter the job location to find jobs.")
+
     
-    if uploaded_file and job_location and remote_only and employment_type:
+    if go and uploaded_file and job_location and remote_only and employment_type:
         resume_text = parse_resume(uploaded_file)
         job_profile = job_profiles(resume_text['text'])
-        experience_years = experience(resume_text['text'])
+        experience_years = extract_experience(resume_text['text'])
 
         st.write(f"### Best Job Profiles for you: {job_profile}")
         st.write(f"### Total Experience: {experience_years}")
@@ -73,9 +71,9 @@ if select == "🔍 Find a Job":
 
 
         # Fetch jobs using extracted skills
-        jobs_set1 = fetch_jobs(job_to_search[0],job_location,remote_only,employment_type,experience_years) 
-        jobs_set2 = fetch_jobs(job_to_search[1],job_location,remote_only,employment_type,experience_years)
-        jobs_set3 = fetch_jobs(job_to_search[2],job_location,remote_only,employment_type,experience_years)
+        jobs_set1 = fetch_jobs(job_to_search[0],job_location,remote_only,employment_type) 
+        jobs_set2 = fetch_jobs(job_to_search[1],job_location,remote_only,employment_type)
+        jobs_set3 = fetch_jobs(job_to_search[2],job_location,remote_only,employment_type)
         all_jobs = jobs_set1 + jobs_set2 + jobs_set3
 
 
@@ -99,12 +97,6 @@ if select == "🔍 Find a Job":
                 .job-details {
                     font-size: 14px;
                     color: #f8f8ff;
-                }
-                .company-logo {
-                    width: 20px;  /* Small Logo Size */
-                    height: 20px;
-                    margin-right: 10px;
-                    vertical-align: middle;
                 }
                 .apply-button {
                     display: inline-block;
@@ -134,9 +126,9 @@ if select == "🔍 Find a Job":
                 with st.container():  # Creates a box around each job listing
                     st.markdown(f"""
                         <div class="job-card">
-                            <p class="job-title">{job['title']}</p>
+                            <p class="job-title">{job['title'] } - {job['company']}</p>
                             <p class="job-details">
-                                <img class="company-logo" src="{job['logo']}" alt="Company Logo"> {job['company']}
+                                Date Posted : {job['date_posted']} on {job['site']}
                             </p>
                             <p class="job-details">📍 {job['location']}</p>
                             <a class="apply-button" href="{job['url']}" target="_blank">🔗 Link to Apply</a>
@@ -148,12 +140,18 @@ if select == "🔍 Find a Job":
 
 
 # for ATS score
-if select == "📊 Get ATS Score":
+if select == "📊 ATS Insights":
+    st.title("📊 ATS Insights")
     resume=st.file_uploader("Upload Your Resume in PDF or in DOCX",type=['pdf','docx'])
     job_description=st.text_area("Paste Job Description")
+    go=st.button("Get Insights")
 
+    if not resume and go:
+        st.warning("Please upload your resume to get insights.")
+    if not job_description and go:
+        st.warning("Please paste the job description to get insights.")
 
-    if resume and job_description:
+    if go and resume and job_description:
         resume_text = parse_resume(resume)
         ats= scoring(resume_text,job_description)
 
@@ -192,15 +190,21 @@ if select == "📊 Get ATS Score":
                 </div>
                 """, unsafe_allow_html=True)
 
-        
-
+     
 
 
 if select == "🤖AI-Powered Resume Customization":
+    st.title("🤖AI-Optimized Resume")
     resume=st.file_uploader("Upload Your Resume in PDF or in DOCX",type=['pdf','docx'])
     job_description=st.text_area("Paste Job Description")
+    go=st.button("Generate Tailored Resume")
 
-    if resume and job_description:
+    if not resume and go:
+        st.warning("Please upload your resume to generate a tailored resume.")
+    if not job_description and go:
+        st.warning("Please paste the job description to generate a tailored resume.")
+
+    if go and resume and job_description:
         resume_text = parse_resume(resume)
         new_tailored_resume=tailored_resume(resume_text,job_description)
         import pdfcrowd
@@ -230,3 +234,53 @@ if select == "🤖AI-Powered Resume Customization":
                                data=f,
                                file_name="Tailored_Resume.pdf",
                                mime="application/pdf")
+
+
+#optimizing linkedin profile
+if select == "🚀LinkedIn Profile Optimizer":
+    st.title("🚀 LinkedIn Profile Optimizer")
+    st.write("Upload your LinkedIn Profile PDF (Go to **your profile → More → Save to PDF**) and get a detailed **review & improvement suggestions**.")
+
+    profile=st.file_uploader("Upload Your Resume in PDF or in DOCX",type=['pdf'])
+    go=st.button("Optimize LinkedIn Profile")
+
+    if not profile and go:
+        st.warning("Please upload your LinkedIn profile to optimize it.")
+    
+    if go and profile:
+        resume_text = parse_resume(profile)
+        suggestions= linkedin_optimization(resume_text)
+        # Custom Styling for a Single Box
+        st.markdown(
+                """
+                <style>
+                    .ats-container {
+                        background-color: #000000;
+                        padding: 20px;
+                        border-radius: 10px;
+                        box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
+                        margin-bottom: 20px;
+                        text-align: left;
+                        font-size: 16px;
+                        line-height: 1.6;
+                    }
+                    .ats-score {
+                        font-size: 22px;
+                        font-weight: bold;
+                        color: #007BFF;
+                   }
+                    .highlight {
+                        color: #28A745;
+                        font-weight: bold;
+                    }
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+        
+        # Display ATS Score and Suggestions Inside a Single Styled Box
+        st.markdown(f"""
+                <div class="ats-container">
+                    {suggestions}
+                </div>
+                """, unsafe_allow_html=True)
